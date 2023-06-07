3f660d11c5441239904605619d6fa32f359e100a
Alejandro <alex@pixelspace.com>
2023-06-07 13:24:57 -0500

    adding streamlit

---
+++ experiments/tiktoken/main.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+st.title("TikToken")
+
+st.write("Hello, world anything else!")
+
+message = st.text_input("This is a text input")
+
+st.write(f"You typed: {message}")
+
+
