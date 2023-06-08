4e2d06df86098c3073e2b93cda3edc953ebd229f
Haley Crabbe <crabbe.haley@gmail.com>
2023-06-08 10:28:46 -0700

    Added input for model and output for number of remaining tokens

--- experiments/tiktoken/main.py
+++ experiments/tiktoken/main.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from token_counter import get_number_tokens_from_openai
+from token_counter import get_number_tokens_from_openai, how_many_tokens_remaining
 
 #title
 col1, col2, col3 = st.columns([1,2,1])
@@ -13,13 +13,22 @@ col11, col22, col23 = st.columns([.1,6,.1])
 with col22:
     st.header(':violet[How many tokens does your message use?]')
 
-#the function is used here
+#find number of tokens used
 message = st.text_area("Message to be tokenized:", height= 10)
 
-encoder = st.selectbox("Select and Encoder:", ['cl100k_base','p50k_base','p50k_edit','r50k_base'])
+encoder = st.selectbox("Select an Encoder:", ['cl100k_base','p50k_base','p50k_edit','r50k_base'])
 
 number_tokens = get_number_tokens_from_openai(message, encoder)
 
 st.header(f'Number of tokens used: :violet[{number_tokens}]')
 
+#finds how many tokens are still available
+model = st.selectbox("Which model are you using?", ["gpt-4 / gpt-4-0314", "gpt-4-32k / gpt-4-32k-0314", "gpt-3.5", "gpt-3.5 (code-davinci-002)", "gpt-3"])
+
+leftover_tokens = how_many_tokens_remaining(number_tokens, model)
+
+if leftover_tokens > 0:
+    st.subheader(f'There are :violet[{leftover_tokens}] tokens available')
+else:
+    st.subheader("Your message uses too many tokens for this model")
 
--- experiments/tiktoken/token_counter.py
+++ experiments/tiktoken/token_counter.py
@@ -2,6 +2,20 @@ import tiktoken
 
 #get_number_tokens_from_openai takes an input message and an encoding, and returns the number of tokens used
 #this uses tiktoken, from openai
-def get_number_tokens_from_openai(input: str, encoding: str) -> str:
-    tokens = tiktoken.get_encoding(encoding).encode(input)
+def get_number_tokens_from_openai(message: str, encoding: str) -> int:
+    tokens = tiktoken.get_encoding(encoding).encode(message)
     return len(tokens)
+
+#takes number of tokens used and the model used, returns the number of tokens left to be used, which can be used in the response
+def how_many_tokens_remaining(tokens_used: int, model: str) -> str:
+    if model == "gpt-4 / gpt-4-0314":
+        leftover_tokens = 8192 - tokens_used
+    elif model == "gpt-4-32k / gpt-4-32k-0314":
+        leftover_tokens = 32768 - tokens_used
+    elif model == "gpt-3.5":
+        leftover_tokens = 4096 - tokens_used
+    elif model == "gpt-3.5 code-davinci-002":
+        leftover_tokens = 8001 - tokens_used
+    else:
+        leftover_tokens = 2049 - tokens_used
+    return leftover_tokens
