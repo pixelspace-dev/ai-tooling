7339ebe22df56be89e1f4948cf33266c2d22de0c
Alejandro <alex@pixelspace.com>
2023-06-08 13:43:42 -0500

    adding test cases

--- experiments/tiktoken/token_counter.py
+++ experiments/tiktoken/token_counter.py
@@ -19,3 +19,34 @@ def how_many_tokens_remaining(tokens_used: int, model: str) -> str:
     else:
         leftover_tokens = 2049 - tokens_used
     return leftover_tokens
+
+
+def how_many_tokens_remaining_as_int(tokens_used: int, model: str) -> int:
+    token_limits = {
+        "gpt-4 / gpt-4-0314": 8192,
+        "gpt-4-32k / gpt-4-32k-0314": 32768,
+        "gpt-3.5": 4096,
+        "gpt-3.5 code-davinci-002": 8001,
+    }
+    
+    leftover_tokens = token_limits.get(model, -1) - tokens_used
+
+    return leftover_tokens
+
+# tests
+import unittest 
+
+class TestHowManyTokensRemainingAsInt(unittest.TestCase):
+
+    def test_known_model(self):
+        self.assertEqual(how_many_tokens_remaining_as_int(1000, "gpt-4 / gpt-4-0314"), 7192)
+        self.assertEqual(how_many_tokens_remaining_as_int(2000, "gpt-4-32k / gpt-4-32k-0314"), 30768)
+        self.assertEqual(how_many_tokens_remaining_as_int(1500, "gpt-3.5"), 2596)
+        self.assertEqual(how_many_tokens_remaining_as_int(6500, "gpt-3.5 code-davinci-002"), 1501)
+
+    def test_unknown_model(self):
+        self.assertEqual(how_many_tokens_remaining_as_int(500, "unknown_model_1"), 1549)
+        self.assertEqual(how_many_tokens_remaining_as_int(1000, "unknown_model_2"), 1049)
+
+if __name__ == '__main__':
+    unittest.main()
