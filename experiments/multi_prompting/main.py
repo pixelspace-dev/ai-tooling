f5f92ca1c59ba94fde60e865409e2a18dc1e2dcd
Haley Crabbe <crabbe.haley@gmail.com>
2023-06-16 11:43:34 -0700

    moved variable declarations

--- experiments/multi_prompting/main.py
+++ experiments/multi_prompting/main.py
@@ -6,6 +6,17 @@ from functions import make_prompt, get_response_with_memory
 
 st.set_page_config(layout="wide")
 
+    # adds the memory to the session state so AI can remember what has been said
+if 'memory' not in st.session_state:
+    st.session_state.memory = ConversationBufferMemory(return_messages=True)
+if 'chat' not in st.session_state:
+    st.session_state.chat = ""
+if 'iteration' not in st.session_state:
+    st.session_state.iteration = 0
+
+response = None
+
+
 col1, col2 = st.columns([2,1], gap="large")
 
 with col2:
@@ -23,17 +34,7 @@ with col1:
     st.subheader("Ask anything!")
 
     #input text to be analyzed
-    user_inquiry = st.text_area("Text input:", height=5)
-    #st.cache(user_inquiry)
-    response = None
-
-    # adds the memory to the session state so AI can remember what has been said
-    if 'memory' not in st.session_state:
-        st.session_state.memory = ConversationBufferMemory(return_messages=True)
-    if 'chat' not in st.session_state:
-        st.session_state.chat = ""
-    if 'iteration' not in st.session_state:
-        st.session_state.iteration = 0
+    user_inquiry = st.text_area("Text input:")
 
     #when button is pressed
     if st.button(':blue[Send]'):
@@ -45,10 +46,10 @@ with col1:
         #update visible chat    
         st.session_state.chat = st.session_state.chat + "User - " + user_inquiry + '\n\n'
         
-        if response and response != "text error":
+        if response:
             st.session_state.chat = st.session_state.chat  + "AI - " + response + '\n\n'
 
-            st.info(st.session_state.chat)
-            
+        st.info(st.session_state.chat)
+
         # hold on to chat memory for entire session
         st.session_state.memory = memory
