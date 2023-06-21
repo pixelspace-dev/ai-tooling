d0c3a328414a9c3f382960efba4059fc5a6b7578
Haley Crabbe <crabbe.haley@gmail.com>
2023-06-21 13:17:30 -0700

    update to token counter app

--- experiments/tiktoken/token_counter.py
+++ experiments/tiktoken/token_counter.py
@@ -14,6 +14,7 @@ def how_many_tokens_remaining_as_int(tokens_used: int, model: str) -> int:
         "gpt-4-32k": 32768,
         "gpt-3.5": 4096,
         "gpt-3.5 (code-davinci-002)": 8001,
+        "gpt-3.5 (turbo-16k)": 16384,
         "gpt-3": 2049,
     }
 
@@ -21,16 +22,13 @@ def how_many_tokens_remaining_as_int(tokens_used: int, model: str) -> int:
 
 # chooses an encode based on the chosen chat model
 # there are different versions of gpt 3.5 that use different encoders - from tiktoken in model.py
-# I set it to cl100k_base - it is most likely that current GPT 3.5 uses this one
-# "gpt-3.5-turbo/-0301": "cl100k_base"
-# "text-davinci-003": "p50k_base",
-# "text-davinci-002": "p50k_base",
-# "code-davinci-002": "p50k_base"
 def set_encoder(model: str) -> str:
     model_options = {
         "gpt-4": "cl100k_base",
         "gpt-4-32k": "cl100k_base",
+        "gpt-3.5": "cl100k_base",
         "gpt-3.5 (code-davinci-002)": "p50k_base",
+        "gpt-3.5 (turbo-16k)": "cl100k_base",
         "gpt-3": "r50k_base",
     }
     return model_options.get(model, "cl100k_base")
